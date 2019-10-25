4ef14ff83e12e9118fb951c51569d3a4670cd3ec
Augustin Gjini <augustin.gjini@pm.me>
2019-10-25 20:00:03 +0200

    capture inside virtual scroller

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,8 @@
 <template>
     <div class="container vertical">
         <div class="container centered">
-            <video id="video"
-                   autoplay
-                   v-if="videoStream"
-                   :srcObject.prop="videoStream"
-                   width="640"
-                   height="480"
-            ></video>
-            <img alt="imgId" id="imgId" :src.prop="captureSrc"/>
+            <video ref="video" autoplay v-if="videoStream" :srcObject.prop="videoStream" width="640"
+                   height="480"></video>
         </div>
         <div class="container centered">
             <button id="takePhotoButtonId" @click="record">Record</button>
@@ -28,12 +22,24 @@
         <div>
             {{ storageDetails }}
         </div>
+        <div class="">
+            <RecycleScroller
+                    direction="vertical"
+                    class="scroller"
+                    :items="captures"
+                    :item-size="captures.length"
+                    v-slot="{ item , index}"
+            >
+                <img :key="index" :src="item.src"/>
+            </RecycleScroller>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue, Watch} from "vue-property-decorator";
     import {Map, Set} from "immutable";
+    import {RecycleScroller} from "vue-virtual-scroller";
 
     class Device {
 
@@ -47,11 +53,19 @@
 
     }
 
-    @Component
+    interface Capture {
+        readonly src: string;
+    }
+
+    @Component({
+        components: {
+            RecycleScroller
+        }
+    })
     export default class HelloWorld extends Vue {
         private videoStream: MediaStream | null = null;
         private storageDetails: string = "";
-        private captureSrc: string = "";
+        private captures: Capture[] = [];
         private cameras: Set<Device> = Set();
         private selectedCamera: string = "";
         private mediaRecorder?: MediaRecorder;
@@ -68,14 +82,18 @@
                         }
                     }
                 );
+            await this.refreshStorage();
+
+            this.openCamera();
+        }
+
+        private async refreshStorage() {
             const {usage, quota} = await navigator.storage.estimate();
             const percentUsed = Math.round(usage!! / quota!! * 100);
             const usageInMib = Math.round(usage!! / (1024 * 1024));
             const quotaInMib = Math.round(quota!! / (1024 * 1024));
 
             this.storageDetails = `${usageInMib} out of ${quotaInMib} MiB used (${percentUsed}%)`;
-
-            this.openCamera();
         }
 
         private openCamera(camera?: string) {
@@ -93,6 +111,26 @@
                 .getUserMedia(constraints)
                 .then(stream => {
                     this.videoStream = stream;
+                    this.mediaRecorder = new MediaRecorder(this.videoStream);
+                    // const chunks: BlobPart[] = [];
+
+                    this.mediaRecorder.onstop = (e: any) => {
+                        /*console.log("data available after MediaRecorder.stop() called.");
+        
+                        const audio = document.createElement('audio');
+                        audio.controls = true;
+                        const blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
+                        const audioURL = window.URL.createObjectURL(blob);
+                        audio.src = audioURL;*
+                        console.log("recorder stopped");*/
+                    };
+
+                    this.mediaRecorder.ondataavailable = (e: any) => {
+                        console.log("Image captured", e);
+                        const src = window.URL.createObjectURL(e.data);
+                        console.log("src", src);
+                        this.captures.push({src: src});
+                    };
                 })
                 .catch(e => {
                     console.log("Unable to read from web-cam ", e);
@@ -108,9 +146,23 @@
         toggleCamera() {
         }
 
-        record() {
-            const imageCapture = new ImageCapture(this.videoStream);
-            //imageCapture.
+        async record() {
+            /*if (this.mediaRecorder.state == "recording") {
+                this.mediaRecorder.stop();
+                console.log(this.mediaRecorder.state);
+            } else {
+                this.mediaRecorder.start();
+                console.log(this.mediaRecorder.state);
+            }*/
+            let canvas = document.createElement("canvas");
+            const video = this.$refs.video;
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext('2d')!.drawImage(video, 0, 0, canvas.width, canvas.height);
+            let src = canvas.toDataURL("image/jpg", 0.1);
+            this.captures.push({id: src, src: src});
+
+            await this.refreshStorage();
         }
     }
 </script>
@@ -126,7 +178,11 @@
 
     .centered {
         justify-content: center;
-        align-items: baseline;
+        align-items: center;
+    }
+
+    .scroller {
+        height: 100%;
     }
 
     h3 {
