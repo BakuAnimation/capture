05124f08e8365ab56fc00527a2dbca9aa2ead2b4
Augustin Gjini <augustin.gjini@pm.me>
2019-10-27 16:32:31 +0100

    remove vue scroller

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import Vue from "vue";
-import VueVirtualScroller from "vue-virtual-scroller";
 import App from "./App.vue";
 import router from "./router";
 
 import store from "./store";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueVirtualScroller);
 
 new Vue({
     router,
