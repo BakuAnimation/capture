2e73cab7150ebbe84bc72772b942715d98c07f97
Jean-Baptiste Gamond <jeanbaptiste.gamond@livingobjects.com>
2019-11-02 18:10:53 +0100

    feat(server): add more messages to socket

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,12 +4,25 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+export interface Offers{
+    [id:string]: RTCSessionDescriptionInit
+}
+
+const offers: Offers = {}
+
 app.use(express.static('../../dist'));
 
 io.on('connection', function (socket) {
+    socket.on('getOffer', function (msg) {
+        console.log(this.offers);
+        socket.emit('rtcOffer', this.offers[msg]);
+    });
     socket.on('rtcAnswer', function (msg) {
         io.to(`${msg.offerer}`).emit('rtcAnswer', msg);
     });
+    socket.on('rtcOffer', function (msg){
+        this.offers[msg.offerer] = msg.offer;
+    })
 });
 
 http.listen(3000, function () {
